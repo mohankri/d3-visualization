698f90e1d0ed9a12ea666ac3527839ced63e40d6
Krishna Mohan <krishnahcdata@hcdatainc.com>
2016-03-10 23:28:01 -0800

    more data point

--- map/drawmap.js
+++ map/drawmap.js
@@ -2,15 +2,18 @@ var drawMap = function() {
 	var map = new google.maps.Map(d3.select("#map").node(), {
 		  zoom: 2,
 		  minZoom:1,
+		  maxzoom:5,
 		  center: new google.maps.LatLng(15, 5),
 		  mapTypeId: google.maps.MapTypeId.TERRAIN,
 	});
-	var radius = 5
+	var radius = 3
+	var country = new Array(255);
+
 	overlay = new google.maps.OverlayView();
 
 	map.addListener("zoom_changed", function(d) {
-		//radius -= map.getZoom();
-		console.log("radius " + map.getZoom());
+		radius = map.getZoom();
+		console.log("radius " + radius);
 		newdata = []
 		overlay.draw();
 		newdata = sample;
@@ -26,7 +29,11 @@ var drawMap = function() {
 
 		overlay.draw = function() {
 			var projection = this.getProjection();
-			var padding = 5;
+			var xpad = new Array(255);
+			var ypad = new Array(255);
+			country.fill(0);
+			xpad.fill(0);
+			ypad.fill(0);
 			//console.log("Draw Certified List " + certifiedList['JAPAN']);
 			//console.log("Draw UnCertified List " + uncertifiedList['JAPAN']);
 			var marker = layer.selectAll("svg")
@@ -48,11 +55,15 @@ var drawMap = function() {
 
 			marker.append("circle")
 			.attr("fill", function(d) {
-				if (d.value[5] == "Certified") 
+				if (d.value[5] == "Certified") {
+					//console.log("Certified ..\n");
 					return "green";
-				else
+				} else {
+					//console.log("Non Certified ..\n");
 					return "red";
+				}
 			})
+			.attr("stroke", "#fff")
 			.attr("r", function(d) {
 				return radius; 
 			})
@@ -62,18 +73,41 @@ var drawMap = function() {
 			.attr("cy", function(d) {
 				return radius + Math.random();
 			});
-
-			function transform(d) {
+			function findIndex(d) {
+				var index = 0;
+				for (var i in geolocation) {
+					if (i == d) {
+						break;
+					}
+					index++;
+				}
+				return index;
+			}
+				
+			function transform(d, i) {
 				if (geolocation[d.value[2]] == undefined) {	
 					console.log("Country " + d.value[2]);
 					return;
 				}
-				console.log("length is %d\n", eval('d.value[2]'.length));
+				if (geolocation[d.value[2]] == "USA") {	
+					console.log("USA ");
+					return;
+				}
+				var index = findIndex(d.value[2]);
+				console.log("index of " + country[index] + " " + d.value[2]);
+				if (country[index] % geolocation[d.value[2]].plotdepth != 0) {
+					xpad[index]+= radius;
+				} else {
+					ypad[index] = country[index]/10 * radius;
+					xpad[index] = radius;
+				}
+				country[index]++;
+				
 				e = new google.maps.LatLng(geolocation[d.value[2]].lat, geolocation[d.value[2]].lng)
 				e = projection.fromLatLngToDivPixel(e);
 				return d3.select(this)
-					.style("left", (e.x - padding) + "px")
-					.style("top", (e.y - padding) + "px");
+					.style("left", (e.x - xpad[index]+1) + "px")
+					.style("top", (e.y - ypad[index]+1) + "px");
 			} 
 		};
 	};
